c3a09e77848c6d28098f9ddd63f0a4b11d8431fb
thaypanyasak <theythey789@gmail.com>
2024-12-17 15:46:46 +0700

    fix css and js

--- js/theme-mode.js
+++ js/theme-mode.js
@@ -1,79 +1,82 @@
 // Kiểm tra và áp dụng theme khi vào trang
-if (
-  localStorage.theme === "dark" ||
-  (!localStorage.theme &&
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
+(function () {
+  // Kiểm tra theme từ localStorage hoặc media query
+  if (
+    localStorage.theme === "dark" ||
+    (!localStorage.theme &&
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 
-// Hàm lấy parameter từ URL
-var getUrlParameter = function getUrlParameter(sParam) {
-  var sPageURL = window.location.search.substring(1),
-    sURLVariables = sPageURL.split("&"),
-    sParameterName,
-    i;
+  // Hàm lấy parameter từ URL
+  var getUrlParameter = function (sParam) {
+    var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split("&"),
+      sParameterName,
+      i;
 
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split("=");
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split("=");
 
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1] === undefined
-        ? true
-        : decodeURIComponent(sParameterName[1]);
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined
+          ? true
+          : decodeURIComponent(sParameterName[1]);
+      }
+    }
+    return false;
+  };
+
+  var version = getUrlParameter("version");
+
+  // Hàm bật theme dark
+  function setDarkTheme() {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+    $("#light_theme").removeClass("active");
+    $("#dark_theme").addClass("active");
+  }
+
+  // Hàm bật theme light
+  function setLightTheme() {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+    $("#dark_theme").removeClass("active");
+    $("#light_theme").addClass("active");
+  }
+
+  // Xử lý khi người dùng click vào chuyển đổi theme
+  function onThemeSwitcherItemClick(e) {
+    var theme = this.dataset.theme;
+    if (theme == "dark") {
+      setDarkTheme();
+    } else {
+      setLightTheme();
     }
   }
-  return false;
-};
 
-var version = getUrlParameter("version");
+  // Lắng nghe sự kiện chuyển đổi theme
+  const themeSwitcherItems = document.querySelectorAll(".switcher-input");
+  themeSwitcherItems.forEach((item) => {
+    item.addEventListener("click", onThemeSwitcherItemClick);
+  });
 
-// Hàm bật theme dark
-function setDarkTheme() {
-  document.documentElement.classList.add("dark");
-  localStorage.theme = "dark";
-  $("#light_theme").removeClass("active");
-  $("#dark_theme").addClass("active");
-}
-
-// Hàm bật theme light
-function setLightTheme() {
-  document.documentElement.classList.remove("dark");
-  localStorage.theme = "light";
-  $("#dark_theme").removeClass("active");
-  $("#light_theme").addClass("active");
-}
-
-// Xử lý khi người dùng click vào chuyển đổi theme
-function onThemeSwitcherItemClick(e) {
-  var theme = this.dataset.theme;
-  if (theme == "dark") {
-    setDarkTheme();
+  // Kiểm tra theme từ localStorage hoặc URL và áp dụng theme mặc định
+  if (localStorage.theme === "dark") {
+    $("#dark_theme").addClass("active");
   } else {
-    setLightTheme();
+    $("#light_theme").addClass("active");
   }
-}
 
-// Lắng nghe sự kiện chuyển đổi theme
-const themeSwitcherItems = document.querySelectorAll(".switcher-input");
-themeSwitcherItems.forEach((item) => {
-  item.addEventListener("click", onThemeSwitcherItemClick);
-});
-
-// Kiểm tra theme từ localStorage hoặc URL và áp dụng theme mặc định
-if (localStorage.theme === "dark") {
-  $("#dark_theme").addClass("active");
-} else {
-  $("#light_theme").addClass("active");
-}
-
-if (version) {
-  if (version == "dark") {
-    setDarkTheme();
-  } else if (version == "light") {
-    setLightTheme();
+  if (version) {
+    if (version == "dark") {
+      setDarkTheme();
+    } else if (version == "light") {
+      setLightTheme();
+    }
   }
-}
+})();
